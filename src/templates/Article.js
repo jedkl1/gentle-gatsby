298cfa37eb01e4d68a2eacfd98425b223740d2dd
Jedrzej Klocek <j.klocek@samsung.com>
2019-05-17 13:31:55 +0200

    [17.05] Fix header arrow animation

--- src/templates/Article.js
+++ src/templates/Article.js
@@ -1,36 +1,49 @@
 import React from "react"
-import { graphql,  } from "gatsby"
+import { graphql } from "gatsby"
 
-const Article = ( { data: { allPrismicArticle }  } ) => {
-
-  console.log(allPrismicArticle)
+const Article = ({ data: { allPrismicArticle } }) => {
+  const [{ data: trip }] = allPrismicArticle.nodes
+  console.log(trip)
   return (
-    <div>
-      adsadas
+    <div className="article">
+      <div className="article__title">{trip.title.text}</div>
+      <div className="article__subtitle">{trip.countries.text}</div>
+      <div className="article__content">
+        <div className="article__content__day">
+
+        </div>
+      </div>
     </div>
   )
 }
 
-export const pageQuery =
-  graphql`
-      query($id: String!) {
-        allPrismicArticle(filter: { id: { eq: $id } }) {
-          edges {
-            node {
-              id
-              slugs
-              data {
-                title {
-                  text
-                }
-                expedition_place {
-                  text
-                }
-              }
-            }
+export const pageQuery = graphql`
+  query($id: String!) {
+    allPrismicArticle(filter: { id: { eq: $id } }) {
+      nodes {
+        id
+        slugs
+        data {
+          title {
+            text
+          }
+          expedition_place {
+            text
+          }
+          image {
+            alt
+            url
+          }
+          countries {
+            text
+          }
+          description {
+            text
           }
         }
       }
-    `
+    }
+  }
+`
 
 export default Article
